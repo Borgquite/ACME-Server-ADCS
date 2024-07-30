496d1b70814bc529b1a5e818766d6c3457d0b9c6
Ottenhus, Thomas <TG85+Github@outlook.de>
2024-07-30 17:46:22 +0200

    CSR Experiment WIP

---
+++ src/ACMEServer/Services/CertRequestValidation/CSRValidationContext.cs
@@ -0,0 +1,97 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Th11s.ACMEServer.Model;
+
+namespace Th11s.ACMEServer.Services.CertRequestValidation
+{
+    internal class CSRValidationContext
+    {
+        public CSRValidationContext(CertificateRequest request, IEnumerable<Identifier> identifiers)
+        {
+            Request = request;
+            IdentifierValidationState = identifiers.ToDictionary(x => x, x => false);
+        }
+
+
+        public CertificateRequest Request { get; }
+
+        public string? SubjectName { get; init; }
+        public IReadOnlyList<string>? CommonNames { get; init; }
+
+        public IReadOnlyList<X509SubjectAlternativeNameExtension>? AlternativeNameExtensions { get; init; }
+        public IReadOnlyList<string>? SubjectAlternativeDNSNames { get; init; }
+
+        public ICollection<Identifier> Identifiers => IdentifierValidationState.Keys;
+        private IDictionary<Identifier, bool> IdentifierValidationState { get; }
+
+
+        internal static CSRValidationContext FromRequestAndOrder(CertificateRequest request, Order order)
+        {
+            var (subjectName, commonNames) = TryParseSubject(request);
+            var alternativeNames = CollectAlternateNames(request);
+
+            var ctx = new CSRValidationContext(request, order.Identifiers)
+            {
+                SubjectName = subjectName,
+                CommonNames = commonNames,
+
+                AlternativeNameExtensions = alternativeNames
+            };
+
+            return ctx;
+        }
+
+
+        public void SetIdentifierToValid(Identifier identifier)
+            => IdentifierValidationState[identifier] = true;
+
+        public bool AreAllIdentifiersValid()
+            => IdentifierValidationState.All(x => x.Value);
+
+        private static (string? subjectName, List<string>? commonNames) TryParseSubject(CertificateRequest request)
+        {
+            try
+            {
+                var subjectName = request.SubjectName;
+
+                if (subjectName == null)
+                    return (null, null);
+
+                var commonNames = subjectName.Name.Split(',', StringSplitOptions.TrimEntries)
+                    .Select(x => x.Split('=', 2, StringSplitOptions.TrimEntries))
+                    .Where(x => string.Equals("cn", x.First(), StringComparison.OrdinalIgnoreCase)) // Check for cn=
+                    .Select(x => x.Last()) // take =value
+                    .ToList();
+
+                return (subjectName.Name, commonNames);
+            }
+            catch
+            {
+                return (null, null);
+            }
+        }
+
+        private static (TODO TODO, TODO TODO) CollectAlternateNames(CertificateRequest request)
+        {
+            var subjectAlternateNames = new List<string>();
+
+            var alternateNameExtensions = request.CertificateExtensions
+                .OfType<X509SubjectAlternativeNameExtension>()
+                .ToList();
+
+            var subjectAlternateNames = alternateNameExtensions
+                .SelectMany(x => x.EnumerateDnsNames())
+                .ToList();
+
+            foreach (var x509Ext in alternateNameExtensions)
+            {
+                var x509extData = x509Ext.RawData[EncodingType.XCN_CRYPT_STRING_BASE64];
+                var alternativeNames = new CX509ExtensionAlternativeNames();
+                alternativeNames.InitializeDecode(EncodingType.XCN_CRYPT_STRING_BASE64, x509extData);
+
+                subjectAlternateNames.AddRange(alternativeNames.AlternativeNames.Cast<CAlternativeName>());
+            }
+
+            return subjectAlternateNames;
+        }
+    }
+}
---
+++ src/ACMEServer/Services/CertRequestValidation/DefaultCSRValidator.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using Th11s.ACMEServer.Model;
+using Th11s.ACMEServer.Model.Services;
+
+namespace Th11s.ACMEServer.Services.CertRequestValidation
+{
+    internal class DefaultCSRValidator : ICSRValidator
+    {
+        private readonly ILogger<DefaultCSRValidator> _logger;
+
+        public DefaultCSRValidator(
+            ILogger<DefaultCSRValidator> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public Task<AcmeValidationResult> ValidateCsrAsync(Order order, string csr, CancellationToken cancellationToken)
+        {
+            _logger.LogDebug($"Attempting validation of CSR {csr}");
+            try
+            {
+                // Load the CSR from the PEM string - this will throw if the CSR is invalid.
+                // The hash algorithm name is not used here, but it is required by the API.
+                var request = CertificateRequest.LoadSigningRequestPem(csr, HashAlgorithmName.SHA256);
+
+                var validationContext = CSRValidationContext.FromRequestAndOrder(request, order);
+
+                var subjectValidator = new SubjectValidator();
+                if (!subjectValidator.IsValid(validationContext))
+                {
+                    _logger.LogDebug("CSR Validation failed due to invalid CN.");
+                    return Task.FromResult(AcmeValidationResult.Failed(new AcmeError("badCSR", "CN Invalid.")));
+                }
+
+                var sanValidator = new AlternateNameValidator();
+                if (!sanValidator.IsValid(validationContext))
+                {
+                    _logger.LogDebug("CSR Validation failed due to invalid SAN.");
+                    return Task.FromResult(AcmeValidationResult.Failed(new AcmeError("badCSR", "SAN Invalid.")));
+                }
+
+                if (!validationContext.AreAllIdentifiersValid())
+                {
+                    _logger.LogDebug("CSR validation failed. Not all identifiers where present in either CN or SAN");
+                    return Task.FromResult(AcmeValidationResult.Failed(new AcmeError("badCSR", "Missing identifiers in CN or SAN.")));
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex, $"Validation of CSR failed with exception.");
+                return Task.FromResult(AcmeValidationResult.Failed(new AcmeError("badCSR", "CSR could not be read.")));
+            }
+
+            _logger.LogDebug("CSR Validation succeeded.");
+            return Task.FromResult(AcmeValidationResult.Success());
+        }
+    }
+}
